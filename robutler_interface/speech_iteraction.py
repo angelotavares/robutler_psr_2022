aa8fb8fffe04061adb97d46317ebbc489d4a635c
angelotavares <mtavares@ua.pt>
2023-01-28 21:33:12 +0000

    add a test version of the speech interaction

---
+++ robutler_interface/speech_iteraction.py
@@ -0,0 +1,43 @@
+import speech_recognition as sr
+from gtts import gTTS
+import os
+import openai
+speech
+# Initialize GPT-3 API key
+openai.api_key = "YOUR_API_KEY"
+
+# Initialize recognizer class (for recognizing the speech)
+r = sr.Recognizer()
+
+# Reading Microphone as source
+# listening the speech and store in audio_text variable
+with sr.Microphone() as source:
+    print("Talk")
+    audio_text = r.listen(source)
+    print("Time over, thanks")
+
+# recoginize_() method will throw a request error if the API is unreachable
+try:
+    # using google speech recognition
+    question = r.recognize_google(audio_text, language ='en-US')
+    print("Question: "+ question)
+except:
+    print("Sorry, I did not get that")
+
+# Use GPT-3 to generate a response to the question
+response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=(question + " "),
+    temperature=0.5,
+    max_tokens=2048
+)
+
+# Get the response text
+text = response.choices[0].text
+
+# Create a gTTS object and save the response to an MP3 file
+tts = gTTS(text=text, lang='en')
+tts.save("response.mp3")
+
+# Play the response
+os.system("mpg321 response.mp3")
